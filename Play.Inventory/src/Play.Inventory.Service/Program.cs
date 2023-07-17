d4cfd74c450d36ca91130a55e4f1d7211a02422d
ngianguyen81 <ngianguyen81@gmail.com>
2023-07-17 15:20:45 +0700

    add retries for inventory to catalog communication

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -2,6 +2,7 @@ using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 builder.Services
     .AddHttpClient<CatalogClient>(client =>
     {
         client.BaseAddress = new Uri("https://localhost:7147");
     })
+    // Handle HTTP 50x, HTTP 408, network failures
+    .AddTransientHttpErrorPolicy(
+        policyBuilder =>
+            policyBuilder
+                // Also retry if failed because of the timeout policy
+                .Or<TimeoutRejectedException>()
+                .WaitAndRetryAsync(
+                    5,
+                    // Exponential backoff: 2 ^ retryAttempt + random
+                    retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                        + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+                //     ,
+                // onRetry: (outcome, timespan, retryAttempt) =>
+                // {
+                //     var serviceProvider = builder.Services.BuildServiceProvider();
+                //     serviceProvider
+                //         .GetService<ILogger<CatalogClient>>()
+                //         ?.LogWarning(
+                //             $"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}"
+                //         );
+                // }
+                )
+    )
     // Wait at most 1 second before timeout
     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
