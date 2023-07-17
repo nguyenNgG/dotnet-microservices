baaf3ee0988d0422f4c35e56a2cdb31ed0c68641
ngianguyen81 <ngianguyen81@gmail.com>
2023-07-17 16:27:14 +0700

    add: inter-service contracts, masstransit, rabbitmq

---
+++ Play.Catalog/src/Play.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,8 @@
+namespace Play.Catalog.Contracts
+{
+    public record CatalogItemCreated(Guid ItemId, string Name, string? Description);
+
+    public record CatalogItemUpdated(Guid ItemId, string Name, string? Description);
+
+    public record CatalogItemDelted(Guid ItemId);
+}
